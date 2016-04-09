3624da46cb5a9a05e3b700902bf54354690bdf1e
taihejin <taihejin2008@126.com>
2016-04-09 23:48:53 +0800

    learning & fighting

---
+++ chapter18/t18.1.3.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <memory>
+#include <algorithm>
+class Base {
+  public:
+    Base(): x_(22) {
+      std::cout << "construct base " << x_ << std::endl;
+    }
+    Base(int x): x_(x) {
+      std::cout << "construct base " << x_ << std::endl;
+    }
+    Base(const Base& base) {
+      x_ = base.x_;
+      std::cout << "copy contruct " << x_ << std::endl;
+    }
+    Base& operator=(const Base& base) {
+      x_ = base.x_;
+      std::cout << "assign contruct " << x_ << std::endl;
+    }
+    ~Base() {
+      std::cout << "destroy base " << x_ << std::endl;
+    }
+  private:
+    int x_;
+};
+
+template <class T> class Vector {
+  public:
+    Vector(): head_(0), free_(0), end_(0) {}
+    ~Vector();
+    void push_back(const T&);
+  private:
+    void reallocate();
+    std::allocator<T> allocator;
+    T* head_;
+    T* free_;
+    T* end_;
+};
+
+template <class T> void Vector<T>::push_back(const T& element) {
+  if (free_ == end_) {
+    reallocate();
+  }
+  new (free_) T(element);
+  free_++;
+}
+
+template <class T> void Vector<T>::reallocate() {
+  std::ptrdiff_t size = free_ - head_;
+  std::ptrdiff_t capacity = 2 * std::max(size, static_cast<std::ptrdiff_t>(1));
+  T* new_head = static_cast<T*>(operator new[](capacity * sizeof(T)));;
+  std::uninitialized_copy(head_, free_, new_head);
+  for (T* p = free_; p != head_; /*empty*/) {
+    (--p)->~T();
+  }
+  if (head_) { // deallocate cannot be called on a 0 pointer
+    operator delete[](head_);
+  }
+  head_ = new_head;
+  free_ = head_ + size;
+  end_ = head_ + capacity;
+}
+
+template <class T> Vector<T>::~Vector() {
+  for (T* p = free_; p != head_; /*empty*/) {
+    (--p)->~T();
+  }
+  if (head_) {
+    operator delete[](head_);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  Vector<Base> vec;
+  Base a(1), b(3), c(5), d(7), e(9);
+  vec.push_back(a);
+  vec.push_back(b);
+  vec.push_back(c);
+  vec.push_back(d);
+  vec.push_back(e);
+  return 0;
+}
---
+++ chapter18/t18.1.7.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <stdlib.h>
+template <class T> class Cache {
+  public:
+    Cache(): next_(0) {}
+    void* operator new (std::size_t);
+    void operator delete (void*, std::size_t);
+    virtual ~Cache() {}
+  protected:
+    T* next_;
+  private:
+    static void attach(T*);
+    static std::allocator<T> allocator;
+    static T* head;
+    static const std::size_t chunk;
+};
+
+template <class T> void* Cache<T>::operator new (std::size_t size) {
+  if (sizeof(T) != size) {
+    throw std::runtime_error("runtime error");
+  }
+  if (!head) { // empty cache
+    T* array = allocator.allocate(chunk);
+    for (int i = 0; i < chunk; ++i) {
+      attach(&array[i]);
+    }
+  }
+  T* p = head;
+  head = head->Cache<T>::next_;
+  return p;
+}
+
+template <class T> void Cache<T>::operator delete (void* element, std::size_t size) {
+  if (element) {
+    attach(static_cast<T*>(element));
+  }
+}
+
+template <class T> void Cache<T>::attach(T* element) {
+  element->Cache<T>::next_ = head;
+  head = element;
+}
+
+template <class T> std::allocator<T> Cache<T>::allocator;
+template <class T> T* Cache<T>::head = 0;
+template <class T> const size_t Cache<T>::chunk = 16;
+
+class Base : public Cache<Base> {
+  public:
+    Base(): x_(0) {
+      std::cout << "construct base " << x_ << std::endl;
+    }
+    Base(int x): x_(x) {
+      std::cout << "construct base " << x_ << std::endl;
+    }
+    ~Base() {
+      std::cout << "destroy base " << x_ << std::endl;
+    }
+  private:
+    int x_;
+};
+
+int main(int argc, char* argv[]) {
+  int times = 16;
+  if (argc == 2) {
+    times = atoi(argv[1]);
+  }
+  Base* array[times];
+  for (int i = 0; i < times; ++i) {
+    array[i] = new Base;
+  }
+  for (int i = 0; i < times; ++i) {
+    delete array[i];
+  }
+
+ return 0; 
+}

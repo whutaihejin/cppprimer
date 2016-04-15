0892fcf80e65eebb26afd407eee7589b775367b3
taihejin <taihejin2008@126.com>
2016-04-15 15:41:48 +0800

    learning & fighting

--- chapter12/build.sh
+++ chapter12/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+# if Makefile already exists, just rename it
 if [ -f Makefile ]; then
-  echo 'find Makefile in current directory, rename it to Makefile.origin instead'
+  echo 'INFO: find Makefile in current directory, rename it to Makefile.origin instead'
   mv Makefile Makefile.origin
 fi
+
+# generate make file body
 source_file_list=$(ls *.cc)
 for file in $source_file_list ;do
   target_file_list="${target_file_list} ${file/.cc/}"
@@ -10,9 +14,10 @@ for file in $source_file_list ;do
 done
 
 echo -e ".PHONY : ALL" >> Makefile
-echo -e "ALL : $target_file_list" >> Makefile
+echo -e "ALL : $target_file_list\n" >> Makefile
 echo -e $target_list >> Makefile
 echo -e ".PHONY : clean" >> Makefile
 echo -e "clean : \n\t rm -rf ${target_file_list}" >> Makefile
 
-echo -e "done sucessfully"
+# tip for user
+echo -e "INFO: done sucessfully"

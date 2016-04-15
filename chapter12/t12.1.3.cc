b7852fc588eda1d5c233ceb75db25d7c1600b3a0
taihejin <taihejin2008@126.com>
2016-04-16 00:12:19 +0800

    12.1.3 -> 5

--- chapter12/t12.1.3.cc
+++ chapter12/t12.1.3.cc
@@ -1,4 +1,6 @@
 #include <iostream>
+
+// third kinds of inline member function
 class Screen {
   public:
     typedef std::string::size_type index_t;
@@ -6,16 +8,22 @@ class Screen {
     char get() const {
       return contents_[cursor_];
     };
-    char get(index_t ht, index_t wd) const {
-      // dummy
-      return contents_[0];
-    }
+    inline char get(index_t ht, index_t wd) const;
+    index_t get_cursor() const;
   private:
     std::string contents_;
     index_t cursor_;
     index_t height_;
     index_t width_;
 };
+
+char Screen::get(index_t r, index_t c) {
+  return content_[0];
+}
+
+inline Screen::get_cursor() const {
+  return cursor_;
+}
 
 int main(int argc, char* argv[]) {
   Screen screen;

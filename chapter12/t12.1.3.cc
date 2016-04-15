6faeca270434a8a775d6ba900a77a1d0e3340b82
taihejin <taihejin2008@126.com>
2016-04-16 00:03:19 +0800

    12.1.3 -> 2

--- chapter12/t12.1.3.cc
+++ chapter12/t12.1.3.cc
@@ -1,11 +1,12 @@
 #include <iostream>
 class Screen {
   public:
+    typedef std::string::size_type index_t;
   private:
     std::string contents_;
-    std::string::size_type cursor;
-    std::string::size_type height_;
-    std::string::size_type width_;
+    index_t cursor;
+    index_t height_;
+    index_t width_;
 };
 
 int main(int argc, char* argv[]) {

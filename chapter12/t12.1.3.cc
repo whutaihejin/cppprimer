7c8fb20bf10f26076895092b48f7a04acf21ac6b
taihejin <taihejin2008@126.com>
2016-04-16 00:08:08 +0800

    learning & fighting

--- chapter12/t12.1.3.cc
+++ chapter12/t12.1.3.cc
@@ -2,14 +2,24 @@
 class Screen {
   public:
     typedef std::string::size_type index_t;
+    Screen(): contents_("whu") {}
+    char get() const {
+      return contents_[cursor_];
+    };
+    char get(index_t ht, index_t wd) const {
+      // dummy
+      return contents_[0];
+    }
   private:
     std::string contents_;
-    index_t cursor;
+    index_t cursor_;
     index_t height_;
     index_t width_;
 };
 
 int main(int argc, char* argv[]) {
   Screen screen;
+  std::cout << screen.get() << std::endl;
+  std::cout << screen.get(0, 0) << std::endl;
   return 0;
 }

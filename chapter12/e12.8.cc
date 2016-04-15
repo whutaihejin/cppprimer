4c321b53314fc4de50d4da4694fa353ecd605690
taihejin <taihejin2008@126.com>
2016-04-15 23:58:07 +0800

    learning & fighting

--- chapter12/e12.8.cc
+++ chapter12/e12.8.cc
@@ -2,19 +2,20 @@
 
 class Sales_item {
   public:
-    inline double avg_price() const {
-      if (units_sold) {
-        return revenue / units_sold;
-      } else {
-        return 0;
-      }
-    }
+    double avg_price() const; 
   private:
     std::string isbn;
     unsigned units_sold;
     double revenue;
 };
 
+inline double Sales_item::avg_price() const {
+  if (units_sold) {
+    return revenue / units_sold;
+  } else {
+    return 0;
+  }
+}
 int main(int argc, char* argv[]) {
   Sales_item item;
   return 0;

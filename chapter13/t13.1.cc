4395b6f58be4e29235ddce0e07648e4f34be0678
taihejin <taihejin2008@126.com>
2016-04-16 22:47:10 +0800

    learning & fighting

--- chapter13/t13.1.cc
+++ chapter13/t13.1.cc
@@ -6,7 +6,7 @@ class Base {
     Base(): x_(0) {
       std::cout << "default constructor " << x_ << std::endl;
     }
-    Base(int x): x_(x) {
+    explicit Base(int x): x_(x) {
       std::cout << "parameter constructor " << x_ << std::endl;
     }
     Base(const Base& base): x_(base.x_) {
@@ -26,10 +26,12 @@ int main(int argc, char* argv[]) {
   Base b(6);
   Base c = b;
   fun(b);
-  Base d = 5;
+  Base d(5);
   std::cout << "--- separate ---" << std::endl;
   Base array[2];
   Base* parray[2];
+  std::cout << "--- separate ---" << std::endl;
+  Base list[3] = {b, d};
   std::cout << "--- separate ---" << std::endl;
   std::vector<Base> vec(2);
   return 0;
